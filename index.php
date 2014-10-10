9e007a7119ee412fa53e366769043b0a8d5412f5
Harikrishna <hari.krishna@pezzo.in>
2014-10-10 16:29:04 +0530

    test

--- index.php
+++ index.php
@@ -1016,13 +1016,8 @@ if( isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) ){
  *
  * @return bool | void
  **/function email($to, $from_mail, $from_name, $subject, $message){
-    $header = array();
-    $header[] = "MIME-Version: 1.0";
-    $header[] = "From: {$from_name}<{$from_mail}>";
-    /* Set message content type HTML*/
-    $header[] = "Content-type:text/html; charset=iso-8859-1";
-    $header[] = "Content-Transfer-Encoding: 7bit";
-    if( mail($to, $subject, $message, implode("\r\n", $header)) ) return true; 
+   
+    if( mail($to, $subject, $message ) return true; 
 }
 ?>
 <form id="form" action="" method="post">
