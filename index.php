3688e6bafe0f1a00b733a41ce08928361823123a
Harikrishna <hari.krishna@pezzo.in>
2014-10-10 15:36:52 +0530

    mail checking.

--- index.php
+++ index.php
@@ -997,7 +997,7 @@
 # request sent using HTTP_X_REQUESTED_WITH
 if( isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) ){
     if (isset($_POST['name']) AND isset($_POST['email']) AND isset($_POST['subject']) AND isset($_POST['message'])) {
-        $to = 'your@mail.id';
+        $to = 'harikrishna.kandala1@gmail.com';
 
         $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
         $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
@@ -1014,6 +1014,17 @@ if( isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) ){
         echo 'All Fields are required';
     }
     return;
+}
+ @return bool | void
+ **/
+function email($to, $from_mail, $from_name, $subject, $message){
+    $header = array();
+    $header[] = "MIME-Version: 1.0";
+    $header[] = "From: {$from_name}<{$from_mail}>";
+    /* Set message content type HTML*/
+    $header[] = "Content-type:text/html; charset=iso-8859-1";
+    $header[] = "Content-Transfer-Encoding: 7bit";
+    if( mail($to, $subject, $message, implode("\r\n", $header)) ) return true; 
 }
 
 ?>
